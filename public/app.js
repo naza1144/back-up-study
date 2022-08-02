434705f81ea968a34522ade49f444aea4e663c9d
naza1144 <85209746+naza1144@users.noreply.github.com>
2022-08-02 18:19:09 +0700

    dm

---
+++ public/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const chalk = require('chalk')
+const app = express();
+const debug = require('debug')('app');
+const mogan = require('morgan');
+const port = 3000;
+
+app.use(mogan('combined'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+
+app.listen(port, () => {
+    console.log("Listening on port" + chalk.green(":" + port));
+})
